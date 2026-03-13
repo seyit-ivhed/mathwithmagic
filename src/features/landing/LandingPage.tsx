091734c79fb2969bedb7f82f9cfdf18c14e53d3f
seyit-ivhed <seyit.ivhed@gmail.com>
2026-03-13 12:33:50 +0100

    feat: add hero collage styles and animations to landing page

--- src/features/landing/LandingPage.tsx
+++ src/features/landing/LandingPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { LandingHero } from './LandingHero';
 import { LandingFeatures } from './LandingFeatures';
 import { LandingFooter } from './LandingFooter';
-import '../premium/components/Premium.css';
 import './LandingPage.css';
 
 export const LandingPage: React.FC = () => (
